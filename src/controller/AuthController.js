abe92cac99b7d8a0f97d30c1c7d4652934f98bd2
Brandon-Neves <84950033+Brandon-Neves@users.noreply.github.com>
2023-04-27 08:43:41 -0300

    Add files via upload

--- src/controller/AuthController.js
+++ src/controller/AuthController.js
@@ -3,7 +3,6 @@ import { v4 as uuid } from 'uuid'
 import db from '../database/database.js'
 
 export async function signUp(req, res) {
-  
   const newUser = res.locals.user
   const passwordHashed = bcrypt.hashSync(newUser.password, 10)
 
@@ -19,16 +18,14 @@ export async function signUp(req, res) {
 }
 export async function login(req, res) {
   const token = uuid()
-
   const user = res.locals.user
-  console.log(user)
 
   try {
-      await db.collection('sessions').insertOne({
-        userId: user._id,
-        token
-      })
-      res.send({ token })
+    await db.collection('sessions').insertOne({
+      userId: user._id,
+      token
+    })
+    res.send({ token })
   } catch (err) {
     console.log(err)
     return res.status(500).send(err)
